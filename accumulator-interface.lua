2594320741ad3cb3f1563890ff18ebb458d9bddc
RedRafe <gabri97.gt+redrafe@gmail.com>
2024-11-18 14:28:05 +0100

    release v2.0.0

--- accumulator-interface.lua
+++ accumulator-interface.lua
@@ -1,6 +1,8 @@
-if not AccumulatorWall then AccumulatorWall = {} end
+if not AccumulatorWall then
+  AccumulatorWall = {}
+end
 
-local path_g = "__accumulator-walls__/graphics/"
+local path_g = '__accumulator-walls__/graphics/'
 
 --[[
   @ ACCUMULATOR
@@ -13,65 +15,53 @@ local path_g = "__accumulator-walls__/graphics/"
 ]]
 local _layers = {
   {
-    filename = path_g .. "transparent.png",
-    priority = "extra-high",
+    filename = path_g .. 'transparent.png',
+    priority = 'extra-high',
     width = 32,
     height = 42,
     shift = util.by_pixel(0.078125, -0.3),
-  }
+  },
 }
 
-local function accumulator_picture() return { layers = _layers } end
---local function accumulator_charge() return { layers = _layers } end
---local function accumulator_reflection() return { pictures = _layers[1] } end
---local function accumulator_discharge() return { layers = _layers } end
+local function accumulator_picture()
+  return { layers = _layers }
+end
+-- local function accumulator_charge() return { layers = _layers } end
+-- local function accumulator_reflection() return { pictures = _layers[1] } end
+-- local function accumulator_discharge() return { layers = _layers } end
 
 local function AccumulatorInterfaceEntity(accumulator)
   return {
-    type = "accumulator",
+    type = 'accumulator',
     name = accumulator.name,
     icon = accumulator.icon,
     icon_size = 64,
-    flags =
-    {
-      "not-on-map",
-      "placeable-off-grid",
-      "not-in-kill-statistics"
-    },
-    collision_box = {{0, 0}, {0, 0}},
-    selection_box = {{0, 0}, {0, 0}},
+    flags = { 'not-on-map', 'placeable-off-grid', 'not-in-kill-statistics', 'hide-alt-info' },
+    collision_box = { { 0, 0 }, { 0, 0 } },
+    selection_box = { { 0, 0 }, { 0, 0 } },
     hidden = true,
-    energy_source =
-    {
-      type = "electric",
+    quality_indicator_scale = 0,
+    energy_source = {
+      type = 'electric',
       buffer_capacity = tostring(accumulator.capacity) .. accumulator.units,
-      usage_priority = "tertiary",
+      usage_priority = 'tertiary',
       input_flow_limit = tostring(accumulator.flow_limit) .. accumulator.fl_units,
-      output_flow_limit = tostring(accumulator.flow_limit) .. accumulator.fl_units
+      output_flow_limit = tostring(accumulator.flow_limit) .. accumulator.fl_units,
     },
     picture = accumulator_picture(),
-    --charge_animation = accumulator_charge(),
-    --water_reflection = accumulator_reflection(),
+    -- charge_animation = accumulator_charge(),
+    -- water_reflection = accumulator_reflection(),
     charge_cooldown = 30,
     discharge_cooldown = 60,
-    working_sound =
-    {
-      sound =
-      {
-        filename = "__base__/sound/accumulator-working.ogg",
-        volume = 0.4
-      },
-      idle_sound =
-      {
-        filename = "__base__/sound/accumulator-idle.ogg",
-        volume = 0.35
-      },
-      --persistent = true,
+    working_sound = {
+      sound = { filename = '__base__/sound/accumulator-working.ogg', volume = 0.4 },
+      idle_sound = { filename = '__base__/sound/accumulator-idle.ogg', volume = 0.35 },
+      -- persistent = true,
       max_sounds_per_type = 3,
       audible_distance_modifier = 0.5,
       fade_in_ticks = 4,
-      fade_out_ticks = 20
-    }
+      fade_out_ticks = 20,
+    },
   }
 end
 
--- accumulator-technology.lua
+++ accumulator-technology.lua
@@ -1,5 +1,6 @@
-
-if not AccumulatorWall then AccumulatorWall = {} end
+if not AccumulatorWall then
+  AccumulatorWall = {}
+end
 
 --[[
   @ TECHNOLOGY_TIER
@@ -16,25 +17,14 @@ if not AccumulatorWall then AccumulatorWall = {} end
 ]]
 local function Technology(technology_tier)
   return {
-    type = "technology",
+    type = 'technology',
     name = technology_tier.name,
     icon_size = 256,
-    icon = "__base__/graphics/technology/electric-energy-acumulators.png",
-    effects =
-    {
-      {
-        type = "unlock-recipe",
-        recipe = technology_tier.name
-      }
-    },
+    icon = '__base__/graphics/technology/electric-energy-acumulators.png',
+    effects = { { type = 'unlock-recipe', recipe = technology_tier.name } },
     prerequisites = technology_tier.prerequisites,
-    unit =
-    {
-      count = technology_tier.count,
-      ingredients = technology_tier.ingredients,
-      time = technology_tier.time
-    },
-    order = "c-e-a",
+    unit = { count = technology_tier.count, ingredients = technology_tier.ingredients, time = technology_tier.time },
+    order = 'c-e-a',
   }
 end
 
