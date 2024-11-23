133b176788aec55ab0ef21cb9a97f4736a4a1f53
RedRafe <gabri97.gt+redrafe@gmail.com>
2024-11-23 18:58:58 +0100

    release v2.0.3

--- control.lua
+++ control.lua
@@ -58,6 +58,7 @@ script.on_event(defines.events.on_player_mined_entity, on_destroy, accumulator_w
 script.on_event(defines.events.on_robot_mined_entity,  on_destroy, accumulator_wall_filters)
 script.on_event(defines.events.on_entity_died,         on_destroy, accumulator_wall_filters)
 script.on_event(defines.events.script_raised_destroy,  on_destroy, accumulator_wall_filters)
+script.on_event(defines.events.on_space_platform_mined_entity, on_destroy, accumulator_wall_filters)
 
 -- @ dolly_event 
 --  .player_index: player_index, The index of the player who moved the entity
